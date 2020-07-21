fd02efd2fb4c2ee891c50724098e681b0610edf0
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-07-21 16:34:43 +0700

    Update BattleFrontierMilestones.ts
    remove placeholders, change stuffs

--- src/scripts/battleFrontier/BattleFrontierMilestones.ts
+++ src/scripts/battleFrontier/BattleFrontierMilestones.ts
@@ -1,18 +1,7 @@
 class BattleFrontierMilestones {
 
     private static readonly milestoneRewards = [
-        {item: 'Pokeball', amount: 10, stage: 10},
-        {item: 'Greatball', amount: 10, stage: 20},
-        {item: 'Ultraball', amount: 10, stage: 30},
-        {item: 'Ultraball', amount: 50, stage: 30},
-        {item: 'Masterball', amount: 1, stage: 50},
-        {item: 'Masterball', amount: 1, stage: 100},
-        {item: 'Eevee', amount: 1, stage: 150},
-        {item: 'Porygon', amount: 1, stage: 200},
-        {item: 'Jynx', amount: 1, stage: 250},
-        {item: 'Lickitung', amount: 1, stage: 300},
-        {item: 'Togepi', amount: 1, stage: 400},
-        {item: 'Beldum', amount: 1, stage: 500},
+        {item: 'Pokeball', amount: 50, stage: 10},
     ];
 
     public static nextMileStone() {
@@ -49,7 +38,7 @@ class BattleFrontierMilestones {
     public static nextMileStoneRewardAmount() {
         const reward = this.nextMileStone();
         if (reward) {
-            return reward.amount;
+            return `${reward.amount}`;
         } else {
             return 0;
         }
