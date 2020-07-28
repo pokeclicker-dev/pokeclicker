ff7205cbe8186e336a8b2b031aa82525f17ca0c5
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-07-28 15:45:50 +0700

    Update BattleFrontierMilestones.ts

--- src/scripts/battleFrontier/BattleFrontierMilestones.ts
+++ src/scripts/battleFrontier/BattleFrontierMilestones.ts
@@ -13,7 +13,7 @@ class BattleFrontierMilestones {
     ];
 
     public static nextMileStone() {
-        return this.milestoneRewards.filter(r => r.stage <= BattleFrontierRunner.stage())[0];
+        return this.milestoneRewards.filter(r => r.stage <= BattleFrontierRunner.stage() && r.stage > BattleFrontier.highestStage())[0];
     }
 
 
