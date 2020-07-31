e944cfe99372b3ea55a6bc86f501c659afd51ace
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-07-31 22:33:09 +0700

    Hmmm

---
+++ other/Map tools/distorsion203.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="distorsion203" tilewidth="16" tileheight="16" tilecount="36" columns="6">
+ <image source="../../Downloads/distorsion203.png" width="96" height="96"/>
+</tileset>
