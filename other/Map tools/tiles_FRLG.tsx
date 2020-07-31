e415a73bc0444a1e8eb2d875e549c0b4868df349
anapple96 <62083669+anapple96@users.noreply.github.com>
2020-07-31 11:39:20 +0700

    fix

--- other/Map tools/tiles_FRLG.tsx
+++ other/Map tools/tiles_FRLG.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.1" name="tiles_FRLG" tilewidth="16" tileheight="16" spacing="1" tilecount="1316" columns="28">
- <image source="pokeclicker/other/Map tools/tiles_FRLG.png" width="477" height="800"/>
+ <image source="../../../../../Downloads/PokeclickerMapFiles/353-3536782_tileset-pokemon-tilesets.png" width="477" height="800"/>
 </tileset>
