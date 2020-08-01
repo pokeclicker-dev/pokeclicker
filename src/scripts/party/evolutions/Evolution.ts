2f04baf20b02d320173c3536858a6c93c9c389c1
Isha Dijcks <ishadijcks@gmail.com>
2020-08-01 13:14:28 +0200

    initial

--- src/scripts/party/evolutions/Evolution.ts
+++ src/scripts/party/evolutions/Evolution.ts
@@ -1,11 +1,9 @@
 abstract class Evolution {
     basePokemon: string;
-    evolvedPokemon: string;
     type: EvolutionType;
 
-    constructor(basePokemon: string, evolvedPokemon: string, type: EvolutionType) {
+    constructor(basePokemon: string, type: EvolutionType) {
         this.basePokemon = basePokemon;
-        this.evolvedPokemon = evolvedPokemon;
         this.type = type;
     }
 
@@ -13,19 +11,21 @@ abstract class Evolution {
         return false;
     }
 
+    abstract getEvolvedPokemon(): string
+
     evolve(notification = false): boolean {
         // This Pokemon is from a region we haven't reached yet
-        if (PokemonHelper.calcNativeRegion(this.evolvedPokemon) > player.highestRegion()) {
+        if (PokemonHelper.calcNativeRegion(this.getEvolvedPokemon()) > player.highestRegion()) {
             return false;
         }
 
         // Notify the player if they haven't already caught the evolution, or notifications are enabled
-        if (!App.game.party.alreadyCaughtPokemonByName(this.evolvedPokemon) || notification) {
-            Notifier.notify({ message: `Your ${this.basePokemon} evolved into a ${this.evolvedPokemon}`, type: GameConstants.NotificationOption.success });
+        if (!App.game.party.alreadyCaughtPokemonByName(this.getEvolvedPokemon()) || notification) {
+            Notifier.notify({ message: `Your ${this.basePokemon} evolved into a ${this.getEvolvedPokemon()}`, type: GameConstants.NotificationOption.success });
         }
 
         const shiny = PokemonFactory.generateShiny(GameConstants.SHINY_CHANCE_STONE);
-        App.game.party.gainPokemonById(PokemonHelper.getPokemonByName(this.evolvedPokemon).id, shiny, true);
+        App.game.party.gainPokemonById(PokemonHelper.getPokemonByName(this.getEvolvedPokemon()).id, shiny, true);
         return shiny;
     }
 
