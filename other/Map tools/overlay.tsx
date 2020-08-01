2ae61b41d3f2d32d64171886ce5543883f62344c
RedSparr0w <admin@redsparr0w.com>
2020-08-01 12:43:23 +1200

    remove overlays

--- other/Map tools/overlay.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.1" name="overlay" tilewidth="90" tileheight="70" tilecount="1" columns="1">
- <image source="../../Downloads/download.png" width="90" height="70"/>
-</tileset>
