c0841efd0d8d723bf9c6ce4cf4aaca31d6e74599
RedSparr0w <admin@redsparr0w.com>
2020-08-08 23:47:56 +1200

    Update quest descriptions

--- src/scripts/quests/questTypes/BuyPokeballsQuest.ts
+++ src/scripts/quests/questTypes/BuyPokeballsQuest.ts
@@ -1,9 +1,9 @@
 /// <reference path="../Quest.ts" />
 
 class BuyPokeballsQuest extends Quest implements QuestInterface {
-    constructor(amount: number, ball: GameConstants.Pokeball, reward: number) {
+    constructor(amount: number, pokeball: GameConstants.Pokeball, reward: number) {
         super(amount, reward);
-        this.description = `Buy ${amount.toLocaleString('en-US')} Pokeballs.`;
-        this.focus = App.game.statistics.pokeballsBought[ball];
+        this.description = `Buy ${amount.toLocaleString('en-US')} ${GameConstants.Pokeball[pokeball]}s.`;
+        this.focus = App.game.statistics.pokeballsBought[pokeball];
     }
 }
