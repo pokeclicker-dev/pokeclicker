e59e6fea7476458462410090492509b92dfaebc8
RedSparr0w <admin@redsparr0w.com>
2020-08-10 11:39:54 +1200

    Allow specifying image for reward

--- src/scripts/battleFrontier/BattleFrontierMilestoneItem.ts
+++ src/scripts/battleFrontier/BattleFrontierMilestoneItem.ts
@@ -3,8 +3,9 @@ class BattleFrontierMilestoneItem extends BattleFrontierMilestone {
     amount: number;
 
     constructor (stage: number, itemName: string, amount: number ) {
-        super(stage, description, () => {});
         const description = `${amount} × ${GameConstants.humanifyString(itemName)}`;
+        const image = `assets/images/items/${itemName.replace(/\W/g, '_')}.png`;
+        super(stage, description, () => {}, image);
 
         this.itemName = itemName;
         this.amount = amount;
