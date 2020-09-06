7544d049195dee1aef9c76b77a05f906cacce197
RedSparr0w <admin@redsparr0w.com>
2020-09-07 02:48:51 +1200

    Add example link to config

--- _config.js
+++ _config.js
@@ -4,5 +4,5 @@ module.exports = {
     GOOGLE_ANALYTICS_ID: false,                                             // optional (false) - Google Analytics tracking ID
     DEV_BANNER: (() => process.env.HEROKU)(),                               // optional (false) - If the development banner should be shown
     DISCORD_CLIENT_ID: false,                                               // optional (false) - Discord bot ID
-    DISCORD_LOGIN_URI: false,                                               // optional (false) - Discord login proxy link
+    DISCORD_LOGIN_URI: false,                                               // optional (false) - Discord login proxy link (example: https://gist.github.com/RedSparr0w/46000c523b21e846adbc912d9800e5b8)
 };
