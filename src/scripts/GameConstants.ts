687f6d3d9666d6ace72731eb609a66d1bc05c5f8
Ultima <69112975+Ultima1990@users.noreply.github.com>
2020-09-20 00:29:02 +0200

    Update GameConstants.ts

--- src/scripts/GameConstants.ts
+++ src/scripts/GameConstants.ts
@@ -746,7 +746,6 @@ namespace GameConstants {
     ];
     
     export const UnovaDungeons = [
-        'Pledge Grove', 
         'Flocessy Ranch',
         'Virbank Complex',    //Optional dungeon, contains no unique Pokémon, safe to scrap
         'Liberty Garden',
